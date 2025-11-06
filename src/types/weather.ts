db2c07c4b3fa19440b1a4f160c10971b5b4e64e3
Hendrik <h.welschoff@gmx.de>
2025-11-06 16:29:59 +0100

    Change types

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,12 +1,13 @@
 export interface CurrentWeather {
-  temperature: number;
-  windspeed: number;
-  weathercode: number;
-  time: string;
+  temperature_2m: number;
+  relative_humidity_2m: number;
+  wind_speed_10m: number;
+  precipitation: number;
+  apparent_temperature: number;
 }
 
 export interface WeatherResponse {
   latitude: number;
   longitude: number;
-  current_weather: CurrentWeather;
+  current: CurrentWeather;
 }
